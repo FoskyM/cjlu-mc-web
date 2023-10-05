66253ec498139314759c7a64c71482de32aff242
FoskyM <i@fosky.top>
2023-10-06 01:20:51 +0800

    fix: repair footer layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,14 @@
 <template>
-  <footer class="bg-gray-200 py-10 px-5 md:px-40 absolute bottom-0 w-full">
+  <footer class="bg-gray-200 py-10 px-5 lg:px-40 absolute bottom-0 w-full">
     <div class="grid grid-cols-3">
       <div class="col-span-3 md:col-span-1">
-        <h3 class="text-xl py-2 font-bold">版权所有</h3>
+        <h3 class="text-xl py-1.5 font-bold">版权所有</h3>
         <p class="text-sm mt-1.5">&copy; Copyright 2022-2023 CJLU MC Server.</p>
         <p class="text-sm mt-1.5">Made with love by <a href="https://fosky.top" target="_blank" class="underline">FoskyM</a>.</p>
       </div>
 
-      <div class="col-span-3 md:col-span-1">
-        <h3 class="text-xl py-2 font-bold">友情链接</h3>
+      <div class="col-span-3 md:col-span-1 mt-2 md:mt-0">
+        <h3 class="text-xl py-1.5 font-bold">友情链接</h3>
         <ul class="text-sm">
           <li v-for="link in links" :key="link.name" class="mt-1.5">
             <a :href="link.url" target="_blank">{{ link.name }}</a>
