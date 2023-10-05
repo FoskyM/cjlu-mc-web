e1fb2a86314b7cc27d323a3eeb1a6479907869ee
FoskyM <i@fosky.top>
2023-10-06 00:55:29 +0800

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
   <header class="bg-gray-100 h-16 flex text-black-800 px-5 shadow-md px-0 md:px-40">
-    <img alt="logo" src="@/assets/logo.jpg" class="h-full hidden" />
-    <h1 class="text-2xl font-bold my-auto ml-2">CJLU MC</h1>
+    <img alt="logo" src="@/assets/logo.jpg" class="h-full mr-2 hidden" />
+    <h1 class="text-2xl font-bold my-auto">CJLU MC</h1>
       <nav class="my-auto ml-5 flex items-center">
         <RouterLink to="/" class="bg-gray-200/75 hover:bg-gray-300 mx-1 px-5 py-2 rounded-lg">首页</RouterLink>
         <RouterLink to="/about" class="bg-gray-200/75 hover:bg-gray-300 mx-1 px-5 py-2 rounded-lg">关于</RouterLink>
@@ -14,8 +10,18 @@ import { RouterLink, RouterView } from 'vue-router'
 
   </header>
 
-  <RouterView />
+  <div class="container">
+    <RouterView />
+  </div>
+
+  <Footer />
+
 </template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Footer from "@/components/Footer.vue";
+</script>
 
 <style scoped>
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer class="bg-gray-200 py-10 px-5 md:px-40 absolute bottom-0 w-full">
+    <div class="grid grid-cols-3">
+      <div class="col-span-3 md:col-span-1">
+        <h3 class="text-xl py-2 font-bold">版权所有</h3>
+        <p class="text-sm">&copy;Copyright 2022-2023 CJLU MC Server.</p>
+      </div>
+
+      <div class="col-span-3 md:col-span-1">
+        <h3 class="text-xl py-2 font-bold">友情链接</h3>
+        <ul class="text-sm">
+          <li v-for="link in links" :key="link.name" class="mt-1.5">
+            <a :href="link.url" target="_blank">{{ link.name }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+const links = [
+  {
+    name: "FoskyM",
+    url: "https://fosky.top"
+  },{
+    name: "量见论坛",
+    url: "https://bbs.cjlu.cc"
+  }
+]
+</script>
+
+<style scoped>
+
+</style>
