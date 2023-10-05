9485c72bc3f54fb247943adcd5d4b3b7fbc7c5ff
FoskyM <i@fosky.top>
2023-10-06 01:39:08 +0800

    fix: footer

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
   <header class="bg-gray-100 h-16 flex text-black-800 px-5 shadow-md px-0 md:px-40">
-    <img alt="logo" src="@/assets/logo.jpg" class="h-full mr-2 hidden" />
+    <img alt="logo" src="@/assets/logo.jpg" class="h-full mr-2 hidden"/>
     <h1 class="text-2xl font-bold my-auto">CJLU MC</h1>
-      <nav class="my-auto ml-5 flex items-center">
-        <RouterLink to="/" class="bg-gray-200/75 hover:bg-gray-300 mx-1 px-5 py-2 rounded-lg">首页</RouterLink>
-        <RouterLink to="/about" class="bg-gray-200/75 hover:bg-gray-300 mx-1 px-5 py-2 rounded-lg">关于</RouterLink>
-        <RouterLink to="/history" class="bg-gray-200/75 hover:bg-gray-300 mx-1 px-5 py-2 rounded-lg">历程</RouterLink>
-      </nav>
+    <nav class="my-auto ml-5 flex items-center">
+      <RouterLink to="/" class="bg-gray-200/75 hover:bg-gray-300 mx-1 px-5 py-2 rounded-lg">首页</RouterLink>
+      <RouterLink to="/about" class="bg-gray-200/75 hover:bg-gray-300 mx-1 px-5 py-2 rounded-lg">关于</RouterLink>
+      <RouterLink to="/history" class="bg-gray-200/75 hover:bg-gray-300 mx-1 px-5 py-2 rounded-lg">历程</RouterLink>
+    </nav>
 
   </header>
 
-  <div class="container">
-    <RouterView />
+  <div class="container ">
+    <RouterView/>
   </div>
 
-  <Footer />
+  <Footer/>
 
 </template>
 
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterLink, RouterView} from 'vue-router'
 import Footer from "@/components/Footer.vue";
 </script>
 
